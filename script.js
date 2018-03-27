bc3c27c4e5fba712a30b51c195f42e7be6cb616d
Vyacheslav <slavbai@gmail.com>
2018-03-27 20:20:29 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+$(document).on('ready', function() {
+	$('.slider').slick({
+		dots: false,
+	    infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 4,
+		        slidesToScroll: 1,
+		        infinite: true
+		      }
+		    },
+		    {
+		      breakpoint: 780,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 550,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 350,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+	    ]
+	});
+
+	$('.sl-big').slick({
+		infinite: true,
+		slidesToShow: 7,
+		slidesToScroll: 1,
+		responsive: [
+		    {
+		      breakpoint: 1200,
+		      settings: {
+		        slidesToShow: 6,
+		        slidesToScroll: 1,
+		        infinite: true
+		      }
+		    },
+		    {
+		      breakpoint: 1000,
+		      settings: {
+		        slidesToShow: 5,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 900,
+		      settings: {
+		        slidesToShow: 4,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 750,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 500,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 1
+		      }
+		    }
+	    ]
+	})
+});
